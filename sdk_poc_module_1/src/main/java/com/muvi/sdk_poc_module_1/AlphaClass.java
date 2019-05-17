654bb90e873aa9d93c2e19113240b9b24888cee0
Piyush Kumar <piyush@muvi.com>
2019-05-17 12:23:50 +0530

    Sample alpha & beta classes

---
+++ sdk_poc_module_1/src/main/java/com/muvi/sdk_poc_module_1/AlphaClass.java
@@ -0,0 +1,12 @@
+package com.muvi.sdk_poc_module_1;
+
+public class AlphaClass {
+
+    public boolean isAlpha() {
+        return true;
+    }
+
+    public boolean isBeta() {
+        return false;
+    }
+}
---
+++ sdk_poc_module_2/src/main/java/com/muvi/sdk_poc_module_2/BetaClass.java
@@ -0,0 +1,12 @@
+package com.muvi.sdk_poc_module_2;
+
+public class BetaClass {
+
+    public boolean isAlpha() {
+        return false;
+    }
+
+    public boolean isBeta() {
+        return true;
+    }
+}
